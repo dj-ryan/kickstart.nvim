acca3e9889caa2e330ba927c0497553adc8c849c
David Ryan <goliathstone4@gmail.com>
2025-04-16 11:01:15 -0700

    fet: make and restor session via mksession

--- init.lua
+++ init.lua
@@ -194,6 +194,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>ms', ':mksession! ~/mysession.vim<CR>', { desc = 'Save session' })
+vim.keymap.set('n', '<leader>rs', ':source ~/mysession.vim<CR>', { desc = 'Restore session' })
 -- NOTE: Some terminals have coliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
